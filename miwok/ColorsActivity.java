d298b369933eff7277cba65198ade4bcd257b936
Shariq Aftab <77614011+Shariqaftab20@users.noreply.github.com>
2021-06-15 12:11:34 +0530

    Add files via upload

---
+++ miwok/ColorsActivity.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    MediaPlayer mMediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+        final ArrayList<word> words = new ArrayList<word>();
+        words.add(new word("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
+        words.add(new word("mustard yellow", "chiwiiṭә", R.drawable.color_mustard_yellow,
+                R.raw.color_mustard_yellow));
+        words.add(new word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow,
+                R.raw.color_dusty_yellow));
+        words.add(new word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new word("brown", "ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new word("gray", "ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new word("black", "kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new word("white", "kelelli", R.drawable.color_white, R.raw.color_white));
+
+
+        // Create an array of words
+        WordAdapter wordAdapter = new WordAdapter(this, words,R.color.category_colors);
+
+        ListView listView = (ListView) findViewById(R.id.grid);
+
+        listView.setAdapter(wordAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Get the {@link Word} object at the given position the user clicked on
+                word currentword = words.get(position);
+
+                // Create and setup the {@link MediaPlayer} for the audio resource associated
+                // with the current word
+
+                mMediaPlayer = MediaPlayer.create(ColorsActivity.this, currentword.getAudioResourceId());
+
+                // Start the audio file
+                mMediaPlayer.start();
+            }
+        });
+    }
+}
---
+++ miwok/PhrasesActivity.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class PhrasesActivity extends AppCompatActivity {
+
+    MediaPlayer mMediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phrases);
+        final ArrayList<word> words = new ArrayList<word>();
+        words.add(new word("Where are you going?", "minto wuksus",
+                R.raw.phrase_where_are_you_going));
+        words.add(new word("What is your name?", "tinnә oyaase'nә",
+                R.raw.phrase_what_is_your_name));
+        words.add(new word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
+        words.add(new word("How are you feeling?", "michәksәs?", R.raw.phrase_how_are_you_feeling));
+        words.add(new word("I’m feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
+        words.add(new word("Are you coming?", "әәnәs'aa?", R.raw.phrase_are_you_coming));
+        words.add(new word("Yes, I’m coming.", "hәә’ әәnәm", R.raw.phrase_yes_im_coming));
+        words.add(new word("I’m coming.", "әәnәm", R.raw.phrase_im_coming));
+        words.add(new word("Let’s go.", "yoowutis", R.raw.phrase_lets_go));
+        words.add(new word("Come here.", "әnni'nem", R.raw.phrase_come_here));
+
+        WordAdapter wordAdapter = new WordAdapter(this, words,R.color.category_phrases);
+        ListView listView = (ListView) findViewById(R.id.grid);
+        listView.setAdapter(wordAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Get the {@link Word} object at the given position the user clicked on
+                word currentword = words.get(position);
+
+                // Create and setup the {@link MediaPlayer} for the audio resource associated
+                // with the current word
+
+                mMediaPlayer = MediaPlayer.create(PhrasesActivity.this, currentword.getAudioResourceId());
+
+                // Start the audio file
+                mMediaPlayer.start();
+            }
+        });
+    }
+}
